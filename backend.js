f53103108cdfa6974f82b2f87576ec0390759ca9
Margaret M Bertoni <bertoni.m@northeastern.edu>
2025-10-09 15:40:57 -0400

    Initializing express backend.js

---
+++ backend.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+console.log("Initializing the backend...");
+// Initialize express
+const app = express();
+const PORT = process.env.PORT || 3000; 
+
+app.use(express.static('frontend'));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+}); 
+
