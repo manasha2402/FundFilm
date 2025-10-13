df33db4357b263b717263a7198df2e2006d8745c
Margaret M Bertoni <bertoni.m@northeastern.edu>
2025-10-12 20:18:02 -0400

    Attempting to switch to atlas

--- db/MyMongoDB.js
+++ db/MyMongoDB.js
@@ -1,9 +1,11 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
 function MyMongoDB() {
   const me = {};
-  const URI = process.env.MONGODB_URI || "mongodb://localhost:27017";
-  const DB_NAME = "pledgeDatabase";
-  const COLLECTION_NAME = "pledges";
+  //const URI = process.env.MONGODB_URI || "mongodb://localhost:27017";
+  const uri = "mongodb+srv://bertoni:fundFilm25@pledgescluster.ajvbfvz.mongodb.net/?retryWrites=true&w=majority&appName=PledgesCluster";
+  const DB_NAME = "FundFilmDB";
+  const COLLECTION_NAME = "pledgeData";
 
   const connect = () => {
     // Connect with client
